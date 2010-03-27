86c61c088021c90b1919333293cd9852ed148659
Marten Veldthuis <marten@veldthuis.com>
2010-03-27 11:43:20 +0100

    remove some old files

--- src/commode/response.clj
+++
@@ -1,7 +0,0 @@
-(ns commode.response
-  (:require clj-record.boot)
-  (:use [commode.config :only (db)]))
-
-(clj-record.core/init-model
- (:associations
-  (belongs-to factoid)))
--- src/commode/trigger.clj
+++
@@ -1,7 +0,0 @@
-(ns commode.trigger
-  (:require clj-record.boot)
-  (:use [commode.config :only (db)]))
-
-(clj-record.core/init-model
- (:associations 
-  (belongs-to factoid)))
