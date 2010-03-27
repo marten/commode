8dcb55fb5e825eabc6ed9366e2eb1bde4e77ff08
Marten Veldthuis <marten@veldthuis.com>
2010-03-27 12:03:21 +0100

    remove stale deps from lein file

--- project.clj
+++ project.clj
@@ -1,11 +1,6 @@
 (defproject commode "0.1"
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-		 [clj-record "1.0-SNAPSHOT"]
                  [org.jibble/pircbot "1.5.0"]
                  [mysql/mysql-connector-java "5.1.6"]
-                 [org.clojars.snuxoll/clojureql "1.0.0"]
-;                 [swank-clojure "1.2.0-break-SNAPSHOT"]
-		]
-;  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
-  :main commode.core)
+                 [org.clojars.snuxoll/clojureql "1.0.0"]])
