46b2f900d6034de6136aef9b1024adc4af4f2bbb
Marten Veldthuis <marten@veldthuis.com>
2010-04-05 17:08:18 +0200

    update lein project file

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [org.jibble/pircbot "1.5.0"]
-                 [mysql/mysql-connector-java "5.1.6"]
-                 [org.clojars.snuxoll/clojureql "1.0.0"]])
+                 [mysql/mysql-connector-java "5.1.6"]])
